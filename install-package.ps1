835aad863b90ff427a5a4d7d7df82a8c5198c080
Mr.Choi <mrchoi@nsr.re.kr>
2021-04-15 16:27:36 +0900

    Rename and update install-package.ps1

---
+++ install-package.ps1
@@ -0,0 +1,47 @@
+#Requires -RunAsAdministrator
+
+## install choco
+$chocoCmd = 'choco'
+if (Get-Command $chocoCmd -errorAction SilentlyContinue)
+{
+    "$chocoCmd exists"
+} else {
+    iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+    Write-Host "You may need to shut down and restart powershell and/or consoles first prior to using choco."
+}
+
+## 
+Write-Host "Preparing..."
+
+$browsers = "googlechrome", "firefox" # "opera"
+
+$pdfs = "foxitreader", "pdfxchangepro" # "adobereader", "pdfcreator", "cutepdf", "calibre", "wkhtmltopdf"
+
+$utils = "7zip", "putty", "sudo",  "sysinternals", "curl", "wget", "procexp", "cpu-z.install", "malwarebytes", "vmwareworkstation"
+         # "openssl.light", "jq", "everything", "openssh", "baretail", "lastpass", "dropbox", "filezilla", 
+
+$gnu = "grep", "awk","sed", "findutils" # ,"gnuwin"
+
+$devels = "git.install", "fiddler", "f.lux"
+         # "git-lfs", "poshgit", "sourcetree", "awscli", "jetbrainstoolbox", "nodejs-lts", "azure-cli", "mysql --version 5.7.18", "redis-64", "graphviz", "postman"
+
+$lang = "python2", "python3" # "pip", "golang", "php --version 7.4.5", "composer"
+
+# $devels_extra = "yarn", "nuget.commandline", "maven", "gradle"
+
+$messengers = "telegram", "slack"
+
+$editors = "vscode", "010editor", "ultraedit", "beyondcompare"  # "vim", "hxd", "neovim", "notepadplusplus.install", "sublimetext3", "babelpad", 
+
+foreach($packages in $browsers, $pdfs, $utils, $gnu, $devels, $editors, $messengers, $lang) # $devels_extra
+{
+    ## install editor
+    Write-Host "Installing $packages"
+    foreach($p in $packages) {
+        choco install $p -y
+    }
+}
+
+# 
+refreshenv
+
